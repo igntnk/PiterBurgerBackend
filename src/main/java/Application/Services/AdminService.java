3e42cc64c0c663cb446f2085c40deefa751b2d0c
igntnk <ighnatenko@vk.com>
2023-11-25 02:20:08 +0300

    Worker,Admin,Manager services

--- src/main/java/Application/Services/AdminService.java
+++ src/main/java/Application/Services/AdminService.java
@@ -1,4 +1,38 @@
 package Application.Services;
 
+import Application.DataBase.Entities.Auth.Credential;
+import Application.DataBase.Entities.Auth.Roles;
+import Application.DataBase.Entities.BaseStatus;
+import Application.DataBase.Entities.User;
+import Application.DataBase.Repository.CredentialRepository;
+import Application.DataBase.Repository.TaskRepository;
+import Application.DataBase.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdminService {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    CredentialRepository credentialRepository;
+
+    public void setStatus(Long task_id,String status){
+        taskRepository.findById(task_id).ifPresent(task -> task.setStatus(BaseStatus.valueOf(status)));
+    }
+
+    public void removeOrder(Long task_id){
+        taskRepository.deleteById(task_id);
+    }
+
+    public void createUser(User user, Credential credential, Roles roles)
+    {
+        userRepository.save(user);
+        credentialRepository.save(credential);
+    }
 }
--- src/main/java/Application/Services/ManagerService.java
+++ src/main/java/Application/Services/ManagerService.java
@@ -1,4 +1,19 @@
 package Application.Services;
 
+import Application.DataBase.Entities.BaseStatus;
+import Application.DataBase.Repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ManagerService {
+
+    TaskRepository taskRepository;
+
+    public void setStatus(Long task_id,String status){
+        taskRepository.findById(task_id).ifPresent(task -> task.setStatus(BaseStatus.valueOf(status)));
+    }
+
+    public void removeOrder(Long task_id){
+        taskRepository.deleteById(task_id);
+    }
 }
