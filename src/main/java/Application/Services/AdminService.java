f01fcdfd796efedb783822de9caf9602950c0bfd
igntnk <ighnatenko@vk.com>
2023-11-26 06:28:47 +0300

    minimal services features

--- src/main/java/Application/Services/AdminService.java
+++ src/main/java/Application/Services/AdminService.java
@@ -1,14 +1,19 @@
 package Application.Services;
 
+import Application.DataBase.Entities.Auth.BaseRole;
 import Application.DataBase.Entities.Auth.Credential;
 import Application.DataBase.Entities.Auth.Roles;
 import Application.DataBase.Entities.BaseStatus;
 import Application.DataBase.Entities.User;
 import Application.DataBase.Repository.CredentialRepository;
+import Application.DataBase.Repository.RolesRepository;
 import Application.DataBase.Repository.TaskRepository;
 import Application.DataBase.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class AdminService {
@@ -22,6 +27,9 @@ public class AdminService {
     @Autowired
     CredentialRepository credentialRepository;
 
+    @Autowired
+    RolesRepository rolesRepository;
+
     public void setStatus(Long task_id,String status){
         taskRepository.findById(task_id).ifPresent(task -> task.setStatus(BaseStatus.valueOf(status)));
     }
@@ -30,9 +38,24 @@ public class AdminService {
         taskRepository.deleteById(task_id);
     }
 
-    public void createUser(User user, Credential credential, Roles roles)
+    public void createUser(User user,Roles role)
     {
         userRepository.save(user);
-        credentialRepository.save(credential);
+        Credential newUserCredential = new Credential(true,"newUser","12345",user,
+                Stream.of(new Roles(BaseRole.CUSTOMER)).collect(Collectors.toSet()));
+    }
+
+    public void changeUserInfo(User user){
+        userRepository.save(user);
+    }
+
+    public void changeUserRole(Long user_id, Roles role){
+        userRepository.findById(user_id).ifPresent(user -> user.getCredential().setRoles(
+                Stream.of(new Roles(BaseRole.CUSTOMER),role).collect(Collectors.toSet())
+        ));
+    }
+
+    public void removeUser(Long user_id){
+        userRepository.deleteById(user_id);
     }
 }
--- src/main/java/Application/Services/UserService.java
+++ src/main/java/Application/Services/UserService.java
@@ -1,13 +1,17 @@
 package Application.Services;
 
+import Application.DataBase.Entities.BaseStatus;
 import Application.DataBase.Entities.Task;
+import Application.DataBase.Entities.User;
 import Application.DataBase.Repository.AdressRepository;
 import Application.DataBase.Repository.TaskItemRepository;
 import Application.DataBase.Repository.TaskRepository;
+import Application.DataBase.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,12 +20,27 @@ public class UserService {
     private TaskRepository taskRepository;
 
     @Autowired
-    private TaskItemRepository taskItemRepository;
-
-    @Autowired
-    private AdressRepository adressRepository;
+    private UserRepository userRepository;
 
     public List<Task> getTasks(Long id){
         return taskRepository.getOrdersByDays(id,7);
+    }
+
+    public Optional<User> getMe(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Task getCurrentTask(Long id){
+       Optional<Task> task= taskRepository.findById(id);
+       BaseStatus  status = task.get().getStatus();
+       if(status.equals(BaseStatus.ACTIVE) ||
+               status.equals(BaseStatus.COOKING) ||
+               status.equals(BaseStatus.COOKED) ||
+               status.equals(BaseStatus.DONE) ||
+               status.equals(BaseStatus.SERVED) ||
+               status.equals(BaseStatus.SERVING)){
+           return task.get();
+       }
+        return new Task();
     }
 }
